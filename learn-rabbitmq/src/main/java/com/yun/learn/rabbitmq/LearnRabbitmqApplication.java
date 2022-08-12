aa08eea9cc839f34ed7d70691c9d8ce16073aa60
Songyunlong <e-yunlong.song>
2022-08-12 18:18:22 +0800

    feat:创建rabbitmq项目用于学习

--- learn-rabbitmq/src/main/java/com/yun/learn/rabbitmq/LearnRabbitmqApplication.java
+++ learn-rabbitmq/src/main/java/com/yun/learn/rabbitmq/LearnRabbitmqApplication.java
@@ -1,13 +1,13 @@
-package com.yun.learn.common;
+package com.yun.learn.rabbitmq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CommonApplication {
+public class LearnRabbitmqApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CommonApplication.class, args);
+        SpringApplication.run(LearnRabbitmqApplication.class, args);
     }
 
 }
